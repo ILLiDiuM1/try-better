21764e92f3dff454d2f2ce8e0a1917e652913b73
ILLiDi <artemprosckura@gmail.com>
2026-05-17 04:53:56 +0300

    Добавление menu для мобильной версии

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState } from "react";
+
 type NavLink = {
   label: string;
   href: string;
@@ -9,11 +12,22 @@ type NavbarProps = {
 };
 
 export function Navbar({ name, links }: NavbarProps) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="flex items-center justify-between">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="rounded-lg border px-3 py-1 text-sm md:hidden"
+        >
+        Menu
+      </button>
       <p className="text-lg font-bold">{name}</p>
 
-      <div className="flex gap-4 text-sm text-gray-600">
+        <div
+          className={`${
+            isOpen ? "flex" : "hidden"
+          } flex-col gap-4 text-sm text-gray-600 md:flex md:flex-row`}
+        >
         {links.map((link) => (
           <a key={link.href} href={link.href}>
             {link.label}
